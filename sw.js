11bff4a02ce16cb1a9f3b5551157908d2403802b
REDNEWT69 <103671138+REDNEWT69@users.noreply.github.com>
2026-09-09 00:17:38 +0200

    Force le nouveau texte de gestion du secteur dans la PWA

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "chef-secteur-sectoradmin1";
+const CACHE_NAME = "chef-secteur-sectoradmin2";
 const APP_SHELL = [
   "./region-stores.js?rev=official20", "./region-stores.css?rev=region18",
   "./",
@@ -36,7 +36,7 @@ const APP_SHELL = [
   "./stores-layout-order.js?rev=20260908-4",
   "./boulanger-national.js?rev=20260908-1",
   "./national-sectors.js?rev=20260908-1",
-  "./sector-admin.js?rev=20260909-1"
+  "./sector-admin.js?rev=20260909-2"
 ];
 const SCOPE = self.registration.scope;
 self.addEventListener('install', event => {
@@ -74,4 +74,3 @@ self.addEventListener('fetch', event => {
     }
   })());
 });
-
