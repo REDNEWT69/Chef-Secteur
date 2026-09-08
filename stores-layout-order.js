0f27a9a1f54beef3b937433808a05f13ce6e92ec
REDNEWT69 <103671138+REDNEWT69@users.noreply.github.com>
2026-09-09 00:17:24 +0200

    Recharge le texte simplifié de gestion du secteur

--- stores-layout-order.js
+++ stores-layout-order.js
@@ -19,7 +19,7 @@
 
   loadScript('boulanger-national-script', './boulanger-national.js?rev=20260908-1', function () {
     loadScript('national-sectors-script', './national-sectors.js?rev=20260908-1', function () {
-      loadScript('sector-admin-script', './sector-admin.js?rev=20260909-1');
+      loadScript('sector-admin-script', './sector-admin.js?rev=20260909-2');
     });
   });
 
