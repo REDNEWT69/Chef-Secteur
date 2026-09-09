a1e4ada278a06b4db3f78814096dc3a1c6cc783a
REDNEWT69 <103671138+REDNEWT69@users.noreply.github.com>
2026-09-09 12:50:07 +0200

    Centralise la révision du build PWA

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "chef-secteur-stable-20260909-4";
+const BUILD_REV = "20260909-5";
+const CACHE_NAME = "chef-secteur-stable-" + BUILD_REV;
 const CORE_SHELL = [
   "./",
   "./index.html",
