696420d6667e8e284a05b5636ae08c639a9b71e1
REDNEWT69 <103671138+REDNEWT69@users.noreply.github.com>
2026-09-23 21:01:58 +0200

    V246B: use numeric build suffix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD_REV = "20260923-weekly-brief-pdf246b";
+const BUILD_REV = "20260923-weekly-brief-pdf246";
 const CACHE_NAME = "chef-secteur-stable-" + BUILD_REV;
 const CORE_SHELL = [
   "./",
